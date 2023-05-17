9dc53a428345b7101a83d77abfc663fdd29f80ab
Igor <sergienko339@gmail.com>
2023-05-17 22:35:45 +0300

    angular

--- Angular/my-first-project/src/app/counter/counter.component.ts
+++ Angular/my-first-project/src/app/counter/counter.component.ts
@@ -9,6 +9,11 @@ export class CounterComponent {
   name: string = 'Igor';
   counter: number = 0;
 
+  value: string = "";
+
+  currentEditingSkillIndex: number | null = null;
+  editingValue: string = "";
+
   skills: string[] = ["JavaScript", "HTML", "CSS", "Angular"];
 
   increment () {
@@ -20,12 +25,34 @@ export class CounterComponent {
   }
 
   addSkill (skill: string) {
+    console.log(skill, "SKILL");
     this.skills.push(skill);
+    this.value = "";
   }
 
   deleteSkill (skill: string) {
      
   }
 
+  editSkill (index: number) {
+    this.currentEditingSkillIndex = index;
+  }
+
+  onInputChange (event: any) {
+    this.value = event.target.value;
+  }
+
+  onEditInputChange (event: any) {
+    this.editingValue = event.target.value;
+  }
+
+  onSaveEdit () {
+    if (this.editingValue) {
+      this.skills[this.currentEditingSkillIndex!] = this.editingValue;
+    }
+
+    this.currentEditingSkillIndex = null;
+    this.editingValue = "";
+  }
   
 }
