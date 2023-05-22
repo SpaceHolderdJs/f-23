21379b99da00724409f37e70c26ee4004f56628f
Igor <sergienko339@gmail.com>
2023-05-22 22:02:20 +0300

    Angular 3.0

--- Angular/my-first-project/src/app/counter/counter.component.ts
+++ Angular/my-first-project/src/app/counter/counter.component.ts
@@ -1,58 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit, DoCheck {
   name: string = 'Igor';
   counter: number = 0;
 
-  value: string = "";
+  value: string = '';
+  searchValue: string = '';
 
   currentEditingSkillIndex: number | null = null;
-  editingValue: string = "";
+  editingValue: string = '';
 
-  skills: string[] = ["JavaScript", "HTML", "CSS", "Angular"];
+  skills: string[] = ['JavaScript', 'HTML', 'CSS', 'Angular'];
 
-  increment () {
+  todos: {
+    id: number;
+    title: string;
+    userId: number;
+    completed: boolean;
+  }[] = [];
+
+  todosSearchResults: typeof this.todos = [];
+
+  // initing of the component (first render)
+  ngOnInit() {
+    console.log('Hello from counter');
+
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.todos = json;
+
+        const localStorageSearchValue: string =
+          localStorage.getItem('searchValue') || '';
+        this.searchValue = localStorageSearchValue;
+
+        this.filterTodos();
+      });
+  }
+
+  // tracking data updates of component
+  ngDoCheck() {
+    console.log('Component has been updated');
+    console.log(this.todos, 'TODOS');
+
+    // this.todos = this.todos.slice(0, 100);
+  }
+
+  onSearchInputChange(event: any) {
+    console.log(event.target.value);
+    this.searchValue = event.target.value;
+    localStorage.setItem('searchValue', this.searchValue);
+    this.filterTodos();
+  }
+
+  filterTodos() {
+    this.todosSearchResults = this.todos.filter(({ title }) =>
+      title.includes(this.searchValue)
+    );
+  }
+
+  increment() {
     this.counter = this.counter + 1;
   }
 
-  decrement () {
+  decrement() {
     this.counter = this.counter - 1;
   }
 
-  addSkill (skill: string) {
-    console.log(skill, "SKILL");
+  addSkill(skill: string) {
+    console.log(skill, 'SKILL');
     this.skills.push(skill);
-    this.value = "";
+    this.value = '';
   }
 
-  deleteSkill (skill: string) {
-     
-  }
+  deleteSkill(skill: string) {}
 
-  editSkill (index: number) {
+  editSkill(index: number) {
     this.currentEditingSkillIndex = index;
   }
 
-  onInputChange (event: any) {
+  onInputChange(event: any) {
     this.value = event.target.value;
   }
 
-  onEditInputChange (event: any) {
+  onEditInputChange(event: any) {
     this.editingValue = event.target.value;
   }
 
-  onSaveEdit () {
+  onSaveEdit() {
     if (this.editingValue) {
       this.skills[this.currentEditingSkillIndex!] = this.editingValue;
     }
 
     this.currentEditingSkillIndex = null;
-    this.editingValue = "";
+    this.editingValue = '';
   }
-  
 }
